c61a0d5af59997267ceb6f4c30344e9f4bd22566
Cody Murray <cdillon.murray@yahoo.com>
2022-05-07 17:41:25 -0500

    updated tag test

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,13 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   Product.findAll({
+    attribute: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
     include: [
       {
         model: Category, 
@@ -27,6 +34,21 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
+  Product.findOne({
+   where: {
+     id: req.params.id
+   }, 
+   include: [
+    {
+      model: Category, 
+      attributes: ['id', 'category_name']
+    },
+    {
+      model: Tag, 
+      attributes: ['id', 'tag_name']
+    }
+  ]
+  })
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
@@ -107,6 +129,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  
 });
 
 module.exports = router;
